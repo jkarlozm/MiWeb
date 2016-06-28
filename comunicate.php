a45a16212210d810ef4792f22d53bea6771d6dfc
Xharly <hades_seismil@yahoo.com.mx>
2016-06-27 19:00:35 -0500

    actualizacion de informacion de contacto y mapa

--- comunicate.php
+++ comunicate.php
@@ -8,7 +8,7 @@
             <div class="row" id="alertaCorreo">                
             </div>
             <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-6">
+                <div class="col-xs-12 col-sm-7 col-md-7">
                     <!--Formulario comunicate-->
                     <p>Escríbeme</p>
                     <p class="text-justify">Cuentame de tu proyecto, dudas, sugerencias ó simplemente
@@ -47,14 +47,48 @@
                       </div>
                     </div>                       
                 </div>
-                <div class="col-xs-12 col-sm-6 col-md-6 datos-contacto">
+                <div class="col-xs-12 col-sm-5 col-md-5 datosContacto">
                     <p>Información de Contacto</p>
-                    <p><span class="glyphicon glyphicon-map-marker"></span> Puebla, México</p>
-                    <p><span class="glyphicon glyphicon-phone"></span> 044-22.23.39.04.74</p>
-                    <p><span class="glyphicon glyphicon-comment"></span> jkarloz2903@gmail.com</p>
-                    <figure>
+                    <div class="well">
+                        <div class="media">
+                            <div class="media-left">
+                                <i class="glyphicon glyphicon-send"></i>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading">Escribeme</h4>
+                                <p>jkarloz2903@gmail.com</p>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div class="well">
+                        <div class="media">
+                            <div class="media-left">
+                                <i class="glyphicon glyphicon-phone"></i>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading">Llamame</h4>
+                                <p>044-22.28.14.32.77</p>
+                            </div>
+                        </div>                        
+                    </div>
+                    <div class="well">
+                        <div class="media">
+                            <div class="media-left">
+                                <i class="glyphicon glyphicon-map-marker"></i>
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading">Ubicación</h4>
+                                <p>Puebla, Pue. / México</p>
+                            </div>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <div class="embed-responsive embed-responsive-16by9">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7543.167271673289!2d-98.14024977380375!3d19.038060236535028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cfc02391be945d%3A0x930be2adb4f61c4c!2sEl+Salvador%2C+Her%C3%B3ica+Puebla+de+Zaragoza%2C+Pue.!5e0!3m2!1ses!2smx!4v1442935154159" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-                    </figure>
+                    </div>                    
                 </div>
             </div>
         </div>
