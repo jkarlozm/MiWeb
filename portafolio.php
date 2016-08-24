579537e28afeeeb67fd178081bebf42d015e5702
Xharly <hades_seismil@yahoo.com.mx>
2016-08-24 12:02:47 -0500

    Modificacion en la estructura de portafolio.php

--- portafolio.php
+++ portafolio.php
@@ -1,19 +1,35 @@
-<?php
-    //Cabecera de pag.
-    include ('librerias/cabecera.php');
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>KarlozWeb | Portafolio</title>
 
-<!--Contenido-->
-<section class="app-seccion imagen">
-    <div class="container">                
-        <div class="row" id="muestraTrabajosPortafolio">
-        	
-        </div>
-        <div class="row text-center">
-            <ul class="pagination" id="paginado"></ul>
-        </div>
-    </div>
-</section>
+        <!-- Hojas de estilo -->
+        <?php include_Once 'include/hojaestilo.php'; ?>
+    </head>
+    <body>
+        <!-- Menú -->
+        <?php include_Once 'include/encabezado.php'; ?>
 
-<!--Pie de pagina-->        
-<?php include ('librerias/pie.php'); ?>
+        <!-- Botón ir arriba -->
+        <?php include_once 'include/botonarriba.php'; ?>
+
+        <!--Contenido-->
+        <section class="app-seccion imagen">
+            <div class="container">                
+                <div class="row" id="muestraTrabajosPortafolio">
+                    
+                </div>
+                <div class="row text-center">
+                    <ul class="pagination" id="paginado"></ul>
+                </div>
+            </div>
+        </section>
+
+        <!-- Pie de Página -->
+        <?php include_Once 'include/pie.php'; ?>
+
+        <!-- Scritps JS -->
+        <?php include_Once 'include/javascript.php'; ?>
+    </body>
+</html>
