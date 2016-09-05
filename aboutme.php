854806879f2ad30876722ab705f86985ea302528
Xharly <hades_seismil@yahoo.com.mx>
2016-09-05 13:42:22 -0500

    Elimine: cabezera y pie, cambie estructura de aboutme

--- aboutme.php
+++ aboutme.php
@@ -1,173 +1,202 @@
-<?php
-    //Cabecera de pag.
-    include ('librerias/cabecera.php');
-?>
-    <!--Contenido-->
-    <section class="app-aboutme">
-    	<div class="container">
-    		<div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <p>Hola, que tal!!</p>
-                    <p>Mi nombre es Juan Carlos pero me conocen mas como Xharly.</p>
-                    <div class="animacion">
-                        <p id="f1">Computación</p>
-                    </div>
-                    <div class="animacion">
-                        <p id="f2">Karate-Do</p>
-                    </div>
-                    <div class="animacion">
-                        <p id="f3">Lectura</p>
-                    </div>
-                    <div class="animacion">
-                        <p id="f4">Música</p>
-                    </div>
-                </div>        			
-    		</div>
-    	</div>
-    </section>
-    <section class="infopersona">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-4 col-md-4">
-                    <figure class="text-center">
-                        <img src="img/karloz.jpg" alt="karloz" class="img-responsive">
-                        <button class="btn btn-info" id="downcurriculum"><span class="glyphicon glyphicon-cloud-download"></span> Hoja de vida</button>
-                    </figure>
-                </div>
-                <div class="col-xs-12 col-sm-8 col-md-8">
-                    <br>
-                    <p class="text-center">Acerca de mi</p>
-                    <p>Saludos!!</p>
-                    <p class="text-justify">Mi nombre es Juan Carlos, soy un fiel apasionado de todo aquello que tenga que ver con los ordenadores, la tecnología, es por ello que me gusta estar informado a través de redes sociales, blogs, foros, revistas para estar en constante capacitación de manera autodidacta.</p>
-                    <p class="text-justify">Titulado como Ingeniero en Tecnologías de la Información y Comunicaciones (TIC's) en la Univesidad Tecnológica de Puebla (2011-2015), tecnología en la cual desde los 16 años me empezo a gustar y me desempeñaba bastante bien.</p>
-                    <p class="text-justify">Vivo en la ciudad de Puebla - me gusta escuchar música (Hip-Hop, RandB, Rap) a todas horas y en todo momento, no soy un apacionado a la lectura, pero si un libro me atrapa, no lo dejo hasta acabarlo, soy de las porsonas que les gusta ver películas en sus ratos de oscio, soy un fan de las películas de acción, futuristas,
-                    pero no me pasan nada las películas cursis.
-                    <br>
-                    ¿Deporte? claro que si, las artes marciales son mi mero mole, actualmente soy cinta morada (8° kyu).
-                    </p>
-                </div>
-            </div>
-            <br>
-            <br>
-            <div class="row text-center">
-                <div class="col-md-4">
-                    <p>Último Libro</p>
-                    <figure>
-                        <img src="img/libro.jpg" alt="Tormenta de Espadas" class="img-responsive img-circle">
-                        <p>Tormenta de Espadas</p>
-                    </figure>                        
-                </div>
-                <div class="col-md-4">
-                    <p>Último Disco</p>
-                    <figure>
-                        <img src="img/disco.jpg" alt="Pandora" class="img-responsive img-circle">
-                        <p>Pandora</p>
-                    </figure>
-                </div>
-                <div class="col-md-4">
-                    <p>Última Película</p>
-                    <figure>
-                        <img src="img/pelicula.jpg" alt="Divergent" class="img-responsive img-circle">
-                        <p>Divergent</p>
-                    </figure>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="habilidades">
-    	<div class="container">
-    		<p>Habilidades</p>
-    		<div class="row">
-    			<div class="col-md-4">
-    				<ul class="list-group">
-                        <li class="list-group-item titulo-lista"><h2>BackEnd</h2></li>
-                        <li class="list-group-item">
-                            PHP
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            MySQL
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            Node.js
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            .NET (C#)
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                    </ul>
-    			</div>
-                <div class="col-md-4">
-                    <ul class="list-group">
-                        <li class="list-group-item titulo-lista"><h2>FrontEnd</h2></li>
-                        <li class="list-group-item">
-                            HTML 5
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            CSS3
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            JavaScript
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            jQuery
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            Bootstrap
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                    </ul>
-                </div> 
-                <div class="col-md-4">
-                    <ul class="list-group">
-                        <li class="list-group-item titulo-lista"><h2>Sistemas Operativos</h2></li>
-                        <li class="list-group-item">
-                            Windows XP <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            Windows Seven <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            Windows 8 <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            Linux (Ubuntu) <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            MAC OS <span class="pull-right glyphicon glyphicon-remove"></span>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <ul class="list-group">
-                        <li class="list-group-item titulo-lista"><h2>Redes</h2></li>
-                        <li class="list-group-item">
-                            CCENT
-                            <span class="pull-right glyphicon glyphicon-remove"></span>
-                        </li>
-                        <li class="list-group-item">
-                            CCNA
-                            <span class="pull-right glyphicon glyphicon-ok"></span>
-                        </li>
-                        <li class="list-group-item">
-                            CCNP
-                            <span class="pull-right glyphicon glyphicon-remove"></span>
-                        </li>
-                        <li class="list-group-item">
-                            CCIE
-                            <span class="pull-right glyphicon glyphicon-remove"></span>
-                        </li>
-                    </ul>
-                </div> 
-    		</div>        		
-    	</div>        	
-    </section>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="es"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="es"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
+    <head>
+        <!-- Meta Etiquetas -->
+        <meta charset="UTF-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="title" content="Karloz Web | Portafolio">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>KarlozWeb | AboutMe</title>
 
-    <!--Pie de pagina-->        
-    <?php include ('librerias/pie.php'); ?>
+        <!-- Hoja de Estilos -->
+        <?php include_Once 'include/hojaestilo.php'; ?>
+    </head>
+    <body>
+        <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!--Menú-->
+        <?php include_Once 'include/encabezado.php'; ?>        
+
+        <!-- Botón ir arriba -->
+        <?php include_Once 'include/botonarriba.php'; ?>
+
+        <!--Contenido-->
+        <section class="app-aboutme">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <p>Hola, que tal!!</p>
+                        <p>Mi nombre es Juan Carlos pero me conocen mas como Xharly.</p>
+                        <div class="animacion">
+                            <p id="f1">Computación</p>
+                        </div>
+                        <div class="animacion">
+                            <p id="f2">Karate-Do</p>
+                        </div>
+                        <div class="animacion">
+                            <p id="f3">Lectura</p>
+                        </div>
+                        <div class="animacion">
+                            <p id="f4">Música</p>
+                        </div>
+                    </div>                  
+                </div>
+            </div>
+        </section>
+        <section class="infopersona">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-sm-4 col-md-4">
+                        <figure class="text-center">
+                            <img src="img/karloz.jpg" alt="karloz" class="img-responsive">
+                            <button class="btn btn-info" id="downcurriculum"><span class="glyphicon glyphicon-cloud-download"></span> Hoja de vida</button>
+                        </figure>
+                    </div>
+                    <div class="col-xs-12 col-sm-8 col-md-8">
+                        <br>
+                        <p class="text-center">Acerca de mi</p>
+                        <p>Saludos!!</p>
+                        <p class="text-justify">Mi nombre es Juan Carlos, soy un fiel apasionado de todo aquello que tenga que ver con los ordenadores, la tecnología, es por ello que me gusta estar informado a través de redes sociales, blogs, foros, revistas para estar en constante capacitación de manera autodidacta.</p>
+                        <p class="text-justify">Titulado como Ingeniero en Tecnologías de la Información y Comunicaciones (TIC's) en la Univesidad Tecnológica de Puebla (2011-2015), tecnología en la cual desde los 16 años me empezo a gustar y me desempeñaba bastante bien.</p>
+                        <p class="text-justify">Vivo en la ciudad de Puebla - me gusta escuchar música (Hip-Hop, RandB, Rap) a todas horas y en todo momento, no soy un apacionado a la lectura, pero si un libro me atrapa, no lo dejo hasta acabarlo, soy de las porsonas que les gusta ver películas en sus ratos de oscio, soy un fan de las películas de acción, futuristas,
+                        pero no me pasan nada las películas cursis.
+                        <br>
+                        ¿Deporte? claro que si, las artes marciales son mi mero mole, actualmente soy cinta morada (8° kyu).
+                        </p>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <p>Último Libro</p>
+                        <figure>
+                            <img src="img/libro.jpg" alt="Tormenta de Espadas" class="img-responsive img-circle">
+                            <p>Tormenta de Espadas</p>
+                        </figure>                        
+                    </div>
+                    <div class="col-md-4">
+                        <p>Último Disco</p>
+                        <figure>
+                            <img src="img/disco.jpg" alt="Pandora" class="img-responsive img-circle">
+                            <p>Pandora</p>
+                        </figure>
+                    </div>
+                    <div class="col-md-4">
+                        <p>Última Película</p>
+                        <figure>
+                            <img src="img/pelicula.jpg" alt="Divergent" class="img-responsive img-circle">
+                            <p>Divergent</p>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="habilidades">
+            <div class="container">
+                <p>Habilidades</p>
+                <div class="row">
+                    <div class="col-md-4">
+                        <ul class="list-group">
+                            <li class="list-group-item titulo-lista"><h2>BackEnd</h2></li>
+                            <li class="list-group-item">
+                                PHP
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                MySQL
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                Node.js
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                .NET (C#)
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <ul class="list-group">
+                            <li class="list-group-item titulo-lista"><h2>FrontEnd</h2></li>
+                            <li class="list-group-item">
+                                HTML 5
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                CSS3
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                JavaScript
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                jQuery
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                Bootstrap
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                        </ul>
+                    </div> 
+                    <div class="col-md-4">
+                        <ul class="list-group">
+                            <li class="list-group-item titulo-lista"><h2>Sistemas Operativos</h2></li>
+                            <li class="list-group-item">
+                                Windows XP <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                Windows Seven <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                Windows 8 <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                Linux (Ubuntu) <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                MAC OS <span class="pull-right glyphicon glyphicon-remove"></span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <ul class="list-group">
+                            <li class="list-group-item titulo-lista"><h2>Redes</h2></li>
+                            <li class="list-group-item">
+                                CCENT
+                                <span class="pull-right glyphicon glyphicon-remove"></span>
+                            </li>
+                            <li class="list-group-item">
+                                CCNA
+                                <span class="pull-right glyphicon glyphicon-ok"></span>
+                            </li>
+                            <li class="list-group-item">
+                                CCNP
+                                <span class="pull-right glyphicon glyphicon-remove"></span>
+                            </li>
+                            <li class="list-group-item">
+                                CCIE
+                                <span class="pull-right glyphicon glyphicon-remove"></span>
+                            </li>
+                        </ul>
+                    </div> 
+                </div>              
+            </div>          
+        </section>
+
+        <!-- Pie de Página -->
+        <?php include_Once 'include/pie.php'; ?>
+
+        <!-- Scritps JS -->
+        <?php include_Once 'include/javascript.php'; ?>
+    </body>
+</html>    
